31429558fee227a411e306aa51962ba25e3da049
Krarneas <af.saulon@gmail.com>
2025-07-01 23:20:44 +0800

    chore/setting-up-react

---
+++ my-react-project/src/Header.jsx
@@ -0,0 +1,20 @@
+//react components are capable of returning a single element, but can also return children elements
+function Header(){
+//combination of js and html language can be written hgere
+
+    return( //can write pure html         
+        <header>
+            <h1>My website</h1>
+            <nav>
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Services</a></li>
+                    <li><a href="#">Contact</a></li>
+                </ul>
+            </nav>
+        </header>
+    );
+}
+
+export default Header
