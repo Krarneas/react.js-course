0c804025f04546a65394a883e8d7d7f862810caa
Krarneas <af.saulon@gmail.com>
2025-07-03 01:25:34 +0800

    feat: learnt propTypes

--- react-cards/src/Student.jsx
+++ react-cards/src/Student.jsx
@@ -1,6 +1,10 @@
-//needs a props paramater, props is a java object
+import PropTypes from 'prop-types'
+//propTypes = a mechanism that ensures that the passed value is the correct data type
+//            age: PropTypes.number
 
-function Student(props){
+
+
+function Student(props){ //needs a props paramater, props is a java object
 
     return(
         <div className="student">
@@ -11,4 +15,10 @@ function Student(props){
     );
 }
 
+//usually for data type checking
+Student.propTypes = {
+    name: PropTypes.string,
+    age: PropTypes.number,
+    isStudent: PropTypes.bool,
+}
 export default Student
