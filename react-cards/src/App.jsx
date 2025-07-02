004439f0f6b5d721ce21fd3d81c4822afd16b55c
Krarneas <af.saulon@gmail.com>
2025-07-03 01:05:59 +0800

    fix: button module file location

--- react-cards/src/App.jsx
+++ react-cards/src/App.jsx
@@ -1,6 +1,10 @@
 //add imports here
 import Card from './Card.jsx'
-import Button from './assets/Button/Button.jsx'
+import Button from './Button/Button.jsx'
+
+//learning props = read only properties that are shared berween components
+//                 a parent component can send data to a child component
+//                 <Component key = value/>
 
 function App() {
   return(
