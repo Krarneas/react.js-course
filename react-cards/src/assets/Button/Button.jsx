9ebd017bb261b763170ad36eb22b77d102589373
Krarneas <af.saulon@gmail.com>
2025-07-03 01:00:30 +0800

    fix: button style

--- react-cards/src/assets/Button/Button.jsx
+++ react-cards/src/assets/Button/Button.jsx
@@ -7,20 +7,21 @@ import styles from './Button.module.css'
 function Button(){
 
     //inline css
-    const styles = {
-        backgroundColor:"hsl(200, 100%, 50%)",
-        color: "white",
-        padding: "10px 20px",
-        borderRadius: "5px",
-        border: "none",
-        cursor: "pointer",
-    }
+    // const styles = {
+    //     backgroundColor:"hsl(200, 100%, 50%)",
+    //     color: "white",
+    //     padding: "10px 20px",
+    //     borderRadius: "5px",
+    //     border: "none",
+    //     cursor: "pointer",
+    // }
 
     return(
         //module css
-        //<button className={styles.button}>Click Me</button>
+        <button className={styles.button}>Click Me</button>
 
-        <button className={styles}>Click Me</button>
+        //inline css
+        //<button className={styles}>Click Me</button>
     );
 }
 
