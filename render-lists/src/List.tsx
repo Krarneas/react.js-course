fdb5dc8017cb890d8ca569870c00b3976139d3c2
Krarneas <af.saulon@gmail.com>
2025-07-16 22:39:43 +0800

    feat: working and displaying arrays
    - map() function reads every item array
    > its like the struct in C or classes in OOP

---
+++ render-lists/src/List.tsx
@@ -0,0 +1,32 @@
+interface Item {
+    id: number;
+    name: string;
+    calories: number;
+}
+
+interface ListProps{
+    items: Item[];
+    category: string;
+}
+
+export default function List({items, category}: ListProps){
+
+    // const fruits = [{id: 1, name: "apple", calories: 95},
+    //                 {id: 2, name: "orange", calories: 45},
+    //                 {id: 3, name: "banana", calories: 105},];
+
+    //fruits.sort((a, b) => a.name.localeCompare(b.name));//alphabetical
+
+    //fruits.sort((a, b) => a.calories - b.calories); //numberic (least to greatest)
+
+                                                //unique id for each element to avoid data conflict
+    const itemList = items.map(item => <li key={item.id}><b>{item.name}</b>: &nbsp; {item.calories}</li>)
+
+    return (
+        <>
+            <h3>{category}</h3>
+            <ol>{itemList}</ol>
+        </>
+    );
+}
+
